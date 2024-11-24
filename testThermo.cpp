fae8dd19d24809a8e9ab59f42e4cf3a63eacd859
chen8891@umn.edu <kc@RadioactiveDevice.>
2024-11-23 20:49:21 -0600

    Added test files

---
+++ testThermo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class particle
+{
+    public:
+    vector<double> p;//Momentum
+    vector<double> q;//Position
+    double m;//Mass
+}
+
+int main()
+{
+    
+}
